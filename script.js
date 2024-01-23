a08c899a437543caeaf27ad44cc0c4d3058f769d
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2024-01-23 16:42:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,72 +1,27 @@
-//your JS code here. If required.
-const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
-
-const images = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
-];
 
 
-const imageLoad=((url) =>{
-	return `<img src='${url} />'`
-}
-				 
-const all= async () => {
+const imageLoad = (url) => {
+  return `<img src='${url}' />`;
+};
 
-	try{
-		const gallery=await Promiseall(images.map((val) => fetch(val.url))) 
-         gallery.forEach((val) => {
-			 output.InsertAdjacentHtml("beforeend", imageload(val.url))
-		 })
-	  return gallery
-		
-		}catch(err){
-		   return "failed to load the images"
-	}
-}
+const all = async () => {
+  try {
+    const gallery = await Promise.all(images.map((val) => fetch(val.url)));
+    gallery.forEach((val) => {
+      output.insertAdjacentHTML("beforeend", imageLoad(val.url));
+    });
 
-btn.addEventListener("click",() =>{
-	images.forEach((val) =>{
-		output.insertAdjacentHtml("beforeend",imageLoad(val.url))
-	})
-})
+    return gallery;
+  } catch (err) {
+    return "failed to load images";
+  }
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-// const imageLoad = (url) => {
-//   return `<img src='${url}' />`;
-// };
-
-// const all = async () => {
-//   try {
-//     const gallery = await Promise.all(images.map((val) => fetch(val.url)));
-//     gallery.forEach((val) => {
-//       output.insertAdjacentHTML("beforeend", imageLoad(val.url));
-//     });
-
-//     return gallery;
-//   } catch (err) {
-//     return "failed to load images";
-//   }
-// };
-
-// btn.addEventListener("click", () => {
-//   images.forEach((val) => {
-//       output.insertAdjacentHTML("beforeend", imageLoad(val.url));
-//     });
-// });
+btn.addEventListener("click", () => {
+  images.forEach((val) => {
+      output.insertAdjacentHTML("beforeend", imageLoad(val.url));
+    });
+});
 
 
 
